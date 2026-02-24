6d0bdcb406ed8535070d14f3f86fccc092158cfd
IronHeart <adarshkumar854858@gmail.com>
2026-02-24 18:49:03 +0530

    Efficient Pdf Viewer

--- app/src/main/java/com/example/readify/presentation/PdfViewerScreen.kt
+++ app/src/main/java/com/example/readify/presentation/PdfViewerScreen.kt
@@ -1,12 +1,16 @@
 package com.example.readify.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.rizzi.bouquet.ResourceType
@@ -18,6 +22,23 @@ fun PdfViewerScreen(
     url : String,
 ) {
     var isDarkMode by remember{ mutableStateOf(false) }
-    val pdfState = rememberVerticalPdfReaderState(resource = ResourceType.Remote(url), isZoomEnable = true)
-    VerticalPDFReader(state = pdfState, modifier = Modifier.fillMaxSize().background(color = Color.Gray))
+    val pdfState = rememberVerticalPdfReaderState(
+        resource = ResourceType.Remote(url),
+        isZoomEnable = true
+    )
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        VerticalPDFReader(
+            state = pdfState,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.DarkGray)
+        )
+
+        //Loader if pdf not shown
+        if (!pdfState.isLoaded) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
 }
